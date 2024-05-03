640e387043517ffeb6a3d7cd14a0f7c21fc6e658
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-05-03 07:57:30 +0530

    TCP Client

--- EXP 3/client.c
+++ EXP 3/client.c
@@ -1,29 +1,29 @@
-#include <arpa/inet.h> // inet_addr()
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h> // bzero()
 #include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h> // inet_addr()
 #include <unistd.h> // read(), write(), close()
+
 #define MAX 80
 #define PORT 8080
-#define SA struct sockaddr
+
 void func(int sockfd)
 {
-	char buff[MAX];
+	char buffer[MAX];
 	int n;
 	for (;;) {
-		bzero(buff, sizeof(buff));
+		bzero(buffer, sizeof(buffer));
 		printf("Enter the string : ");
 		n = 0;
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		write(sockfd, buff, sizeof(buff));
-		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
-		printf("From Server : %s", buff);
-		if ((strncmp(buff, "exit", 4)) == 0) {
+		while ((buffer[n++] = getchar()) != '\n');
+		write(sockfd, buffer, sizeof(buffer));
+		bzero(buffer, sizeof(buffer));
+		read(sockfd, buffer, sizeof(buffer));
+		printf("From Server : %s", buffer);
+		if ((strncmp(buffer, "exit", 4)) == 0) {
 			printf("Client Exit...\n");
 			break;
 		}
@@ -33,26 +33,25 @@ void func(int sockfd)
 int main()
 {
 	int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
+	struct sockaddr_in server, client;
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		printf("socket creation failed...\n");
 		exit(0);
-	}
-	else
+	} else
 		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
+
+	bzero(&server, sizeof(server));
 
 	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(PORT);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(PORT);
 
 	// connect the client socket to server socket
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-		!= 0) {
+	if (connect(sockfd, (struct sockaddr * ) & server, sizeof(server)) != 0) {
 		printf("connection with the server failed...\n");
 		exit(0);
 	}
@@ -65,3 +64,15 @@ int main()
 	// close the socket
 	close(sockfd);
 }
+
+/*
+joswin@LAPTOP:~/TCP$ gcc client.c -o client
+joswin@LAPTOP:~/TCP$ ./client 
+Socket successfully created..
+connected to the server..
+Enter the string : Message 1
+From Server : Message 2
+Enter the string : Message 3
+From Server : exit
+Client Exit...
+*/
